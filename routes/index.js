5c31f9edb699725e0e5f571cbb736b8fced067f4
LeontiBrechko <leontibrechko@gmail.com>
2017-03-09 19:40:57 -0500

    added authentication routes

--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,7 @@
  * @description Personal Portfolio website component with Business Contact List – Authentication
  */
 
+// modules required for routing
 let express = require('express');
 let nodemailer = require('nodemailer');
 let config = require('config');
@@ -14,7 +15,8 @@ let router = express.Router();
 /* GET home page. */
 router.get('/', (req, res, next) => {
     res.render('index', {
-        title: 'Home'
+        title: 'Home',
+        displayName: req.user ? req.user.displayName : ''
     });
 });
 
@@ -92,6 +94,88 @@ router.post('/contact', (req, res, next) => {
             })
         }
     });
+});
+
+//------------------------------------AUTHORIZATION ROUTES------------------------------------
+let passport = require('passport');
+
+// defining the user model
+let User = require('../models/users');
+
+// GET login request
+router.get('/login', (req, res, next) => {
+    // check to see if the user is not already logged in
+    if (!req.user) {
+        // render the login page
+        res.render('auth/login', {
+            title: "Login",
+            businessContacts: {},
+            messages: req.flash('loginErrorMessage'),
+            displayName: req.user ? req.user.displayName : ''
+        });
+        return;
+    } else {
+        return res.redirect('/businessContacts');
+    }
+});
+
+// POST login request
+router.post('/login', passport.authenticate('local', {
+    successRedirect: '/businessContacts',
+    failureRedirect: '/login',
+    failureFlash: {type: 'loginErrorMessage', message: 'Invalid username or password.'}
+}));
+
+// GET register request
+router.get('/register', (req, res, next) => {
+    // check to see if the user is not already logged in
+    if (!req.user) {
+        // render the registration page
+        res.render('auth/register', {
+            title: "Register",
+            businessContacts: {},
+            messages: req.flash('registerMessage'),
+            displayName: req.user ? req.user.displayName : ''
+        });
+        return;
+    } else {
+        return res.redirect('/businessContacts');
+    }
+});
+
+// POST register request
+router.post('/register', (req, res, next) => {
+    User.register(
+        new User({
+            username: req.body.username,
+            password: req.body.password,
+            email: req.body.email,
+            displayName: req.body.displayName
+        }),
+        req.body.password,
+        (err) => {
+            if (err) {
+                console.log('Error inserting new user');
+                if (err.name == "UserExistsError") {
+                    req.flash('registerMessage', 'Registration Error: User Already Exists');
+                }
+                return res.render('auth/register', {
+                    title: "Register",
+                    messages: req.flash('registerMessage'),
+                    displayName: req.user ? req.user.displayName : ''
+                });
+            }
+            // if registration is successful
+            return passport.authenticate('local')(req, res, () => {
+                res.redirect('/businessContacts');
+            });
+        });
+});
+
+// GET logout request
+router.get('/logout', (req, res, next) => {
+    req.logout();
+    res.redirect('/');
 });
 
 module.exports = router;
