70c33d720bd82eb11a2bf1f449b80505f469ffb3
LeontiBrechko <leontibrechko@gmail.com>
2017-03-09 20:10:01 -0500

    added db config

---
+++ config/db.js
@@ -0,0 +1,13 @@
+/**
+ * @author Liavontsi Brechka
+ * @studentID 300800345
+ * @date March 6, 2017
+ * @description Personal Portfolio website component with Business Contact List – Authentication
+ */
+
+module.exports = {
+    //local MongoDB deployment ->
+    // "URI": "mongodb://localhost/assignment2"
+    //Remote MongoDB deployment
+    "URI": "mongodb://leontibrechko:123456@ds145329.mlab.com:45329/assignment2"
+};
